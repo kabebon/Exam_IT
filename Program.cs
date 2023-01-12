2d6a9b9f5813179cf9eac9286cfc0fc9dacaece5
kabebon <professor0123@yandex.ru>
2023-01-12 19:52:13 +0500

    Created GetArray method

--- Program.cs
+++ Program.cs
@@ -2,6 +2,13 @@
 {
     public static void Main(string[] args)
     {
+        Console.Write("Введите строки массива через пробел: ");
+        args = GetArray();
+    }
 
+    private static string[] GetArray()
+    {
+        string[] array = Console.ReadLine().Split(' ');
+        return array;
     }
 }
