c927f56aeabad96169e9277d8f75f63fec78bcf4
kabebon <professor0123@yandex.ru>
2023-01-12 19:54:45 +0500

    Created PrintArray method

--- Program.cs
+++ Program.cs
@@ -11,4 +11,14 @@
         string[] array = Console.ReadLine().Split(' ');
         return array;
     }
+
+    private static void PrintArray(string[] args)
+    {
+        Console.Write("[ ");
+        for (int i = 0; i < args.Length; i++)
+        {
+            Console.Write(args[i] + " ");
+        }
+        Console.Write("]");
+    }
 }
